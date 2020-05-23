8aeac77f5a8d97d602df7e96d59e9791cdc080a6
Ashish Kumar <akmashish15@gmail.com>
2020-05-23 21:22:31 +0530

    Added all modules

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+use phplatest\Member;
+
+session_start();
+?>
+<HTML>
+<HEAD>
+<TITLE>user-registration</TITLE>
+<link href="./assets/css/phplatest-style.css" type="text/css"
+	rel="stylesheet" />
+<link href="./assets/css/user-registration.css" type="text/css"
+	rel="stylesheet" />
+<script src="./vendor/jquery/jquery-3.3.1.js" type="text/javascript"></script>
+</HEAD>
+<BODY>
+	<div class="phplatest-container">
+	<?php require_once "login-form.php";?>
+	</div>
+</BODY>
+</HTML>
